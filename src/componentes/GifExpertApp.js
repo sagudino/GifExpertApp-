355c218d9953c904dcd567d3f857dd41dba63496
Samuel Agudiño <samuel.agudino@gmail.com>
2021-12-10 20:01:20 +0100

    Sección 70

--- src/componentes/GifExpertApp.js
+++ src/componentes/GifExpertApp.js
@@ -1,10 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 export const GifExpertApp = () => {
+  //const categories = ['Categoría 1', 'Categoría 2', 'Categoría 3'];
+  const [categories, setCategories] = useState([
+    'Categoría 1',
+    'Categoría 2',
+    'Categoría 3',
+  ]);
+
+  const anadirCategoria = () => {
+    setCategories([...categories, 'Categoría 4']);
+  };
   return (
-    <>
+    <div className='container'>
       <h1>GifExpertApp</h1>
       <hr />
-    </>
+      <button onClick={anadirCategoria}>Agregar</button>
+      <ul>
+        {categories.map((cat) => {
+          return <li key={cat}>{cat}</li>;
+        })}
+      </ul>
+    </div>
   );
 };
