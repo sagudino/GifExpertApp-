7bc3653848a09b2e17b2775a8f45dd43bcb2349d
Samuel Agudiño <samuel.agudino@gmail.com>
2022-01-04 17:19:15 +0100

    Pruebas en GifGridItem con propTypes

--- src/tests/components/GifGridItem.test.js
+++ src/tests/components/GifGridItem.test.js
@@ -3,8 +3,10 @@ import { shallow } from 'enzyme';
 import { GifGridItem } from '../../componentes/GifGridItem';
 
 describe('Pruebas en GifGridItem', () => {
-  test('Debe de mostra el componente correctamente ', () => {
-    const wrapper = shallow(<GifGridItem />);
+  test('Debe de mostra el componente correctamente', () => {
+    const title = 'Un título';
+    const url = 'https://www.google.com';
+    const wrapper = shallow(<GifGridItem title={title} url={url} />);
     expect(wrapper).toMatchSnapshot();
   });
 });
