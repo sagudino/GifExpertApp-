5dcf5041d9bbc534cd04402c1f847b6183cf587d
Samuel Agudiño <samuel.agudino@gmail.com>
2022-01-04 18:45:59 +0100

    Prueba de enviar el formulario y llamar a la función

--- src/tests/components/AddCategory.test.js
+++ src/tests/components/AddCategory.test.js
@@ -24,9 +24,25 @@ describe('Pruebas en AddCategory', () => {
     input.simulate('change', { target: { value: value } });
   });
 
-  //envío del formulario
+  //envío del formulario, no debe ejecutar la función
   test('NO debe de enviar la información en el submit', async () => {
     wrapper.find('form').simulate('submit', { preventDefault: () => {} });
     expect(setCategories).not.toBeCalled();
+  });
+
+  //envío del formulario, debe ejecutar la función
+  test('Debe de llamar el setCategories y limpiar la caja de texto', async () => {
+    // 1. Simular el inputChange
+    const input = wrapper.find('input');
+    const value = 'Hola mundo';
+    input.simulate('change', { target: { value: value } });
+    // 2. Simular el envío del formulario
+    wrapper.find('form').simulate('submit', { preventDefault: () => {} });
+    // 3. setCategories se debe haber llamado
+    expect(setCategories).toHaveBeenCalled();
+    expect(setCategories).toHaveBeenCalledTimes(1);
+    expect(setCategories).toHaveBeenCalledWith(expect.any(Function));
+    //4. el valor del input debe estar vacío.
+    expect(input.prop('value').trim()).toBe('');
   });
 });
